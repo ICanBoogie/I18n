b3513cf4533f8025d64c2583ae7ccef98af30976
Olivier Laviale <olivier.laviale@gmail.com>
2015-09-02 23:03:52 +0200

    Added message catalog

---
+++ locale/en.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+	'label' => [
+
+		'cancel' => 'Cancel',
+		'continue' => 'Continue',
+		'edit' => 'Edit',
+		'delete' => 'Delete',
+		'display' => 'Display',
+		'download' => 'Download',
+		'remove' => 'Remove',
+		'save' => 'Save',
+		'send' => 'Send',
+		'use' => 'Use'
+
+	],
+
+	'permission' => [
+
+		'none' => 'None',
+		'access' => 'Access',
+		'create' => 'Create',
+		'maintain' => 'Maintain',
+		'manage' => 'Manage',
+		'administer' => 'Administer'
+
+	],
+
+	'label.salutation' => 'Salutation',
+
+	'salutation' => [
+
+		'misses' => 'Misses',
+		'miss' => 'Miss',
+		'mister' => 'Mister'
+
+	]
+];
